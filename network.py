75529ecec57339fe13900497965d01e957a7e11a
pengu2004 <u2208069@rajagiri.edu.in>
2025-09-01 19:51:00 +0530

    Fix variable name typo in create_socket function for consistency

--- network.py
+++ network.py
@@ -7,10 +7,18 @@ BROADCAST_INTERVAL = 3
 BROADCAST_MESSAGE=b'DISCOVERY'
 
 def create_socket():
-    brodcaster=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #DGRAM refers to udp
-    brodcaster.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1) #Letting the socket make brodcasts
-    brodcaster.settimeout(2) #wait for 2 seconds and if nothig happens then ->timeout
-    return brodcaster
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #DGRAM refers to udp
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1) #Letting the socket make brodcasts
+    sock.settimeout(2) #wait for 2 seconds and if nothig happens then ->timeout
+    return sock
+
+def are_you_there(peerlist): #use to add new peers or check if the existing ones are there
+    broadcaster=create_socket()
+    while True:
+        broadcaster.sendto(BROADCAST_MESSAGE,('255,255,255,255',BROADCAST_PORT))
+        print("Broadcaster is online")
+        time.sleep(BROADCAST_INTERVAL)
+
 
 
 
